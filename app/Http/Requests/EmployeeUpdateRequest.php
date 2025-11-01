d77c9dd5938062ba3227a5f86d04b05f211f222a
neveleneve <pandupandu813@gmail.com>
2025-11-01 14:35:24 +0700

    d

--- app/Http/Requests/EmployeeUpdateRequest.php
+++ app/Http/Requests/EmployeeUpdateRequest.php
@@ -17,7 +17,7 @@ class EmployeeUpdateRequest extends FormRequest
 
     public function rules()
     {
-        $karyawanId = $this->route('employee')->id;
+        $karyawanId = $this->route('employee')->employee;
 
         return [
             'name'                  => ['nullable', 'string', 'max:255'],
