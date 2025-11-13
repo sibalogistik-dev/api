43dc19eafec4be29ee2c8fc979f034709b3841c3
neveleneve <pandupandu813@gmail.com>
2025-11-13 17:43:36 +0700

    Implement Sick Note feature: add controller, model, migration, and seeder for sick notes management.

---
+++ app/Http/Controllers/SickNoteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SickNote;
+use Illuminate\Http\Request;
+
+class SickNoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SickNote $sickNote)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SickNote $sickNote)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SickNote $sickNote)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SickNote $sickNote)
+    {
+        //
+    }
+}
