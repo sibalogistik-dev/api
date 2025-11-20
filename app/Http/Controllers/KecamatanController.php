957f049dec8cf0bf4b7169e1874a59e36897ff8d
neveleneve <pandupandu813@gmail.com>
2025-11-20 17:29:09 +0700

    Refactor Kecamatan and Kelurahan controllers to implement request validation, enhance data retrieval with pagination, and improve error handling for create, update, and delete operations.

--- app/Http/Controllers/KecamatanController.php
+++ app/Http/Controllers/KecamatanController.php
@@ -2,34 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ApiResponseHelper;
+use App\Http\Requests\DistrictIndexRequest;
+use App\Http\Requests\DistrictStoreRequest;
 use App\Models\District;
-use App\Models\Kecamatan;
+use App\Services\DistrictService;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class KecamatanController extends Controller
 {
-    public function index()
+    protected $districtService;
+
+    public function __construct(DistrictService $districtService)
     {
-        //
+        $this->districtService = $districtService;
     }
 
-    public function store(Request $request)
+    public function index(DistrictIndexRequest $request)
     {
-        //
+        try {
+            $validated              = $request->validated();
+            $districtQuery          = District::query()->filter($validated);
+            $district               = isset($validated['paginate']) && $validated['paginate'] ? $districtQuery->paginate($validated['perPage'] ?? 10) : $districtQuery->get();
+            $itemsToTransform       = $district instanceof LengthAwarePaginator ? $district->getCollection() : $district;
+            $transformedDistrict    = $itemsToTransform->map(function ($item) {
+                return [
+                    'id'            => $item->id,
+                    'name'          => $item->name,
+                ];
+            });
+            if ($district instanceof LengthAwarePaginator) {
+                return ApiResponseHelper::success('District list', $district->setCollection($transformedDistrict));
+            } else {
+                return ApiResponseHelper::success('District list', $transformedDistrict);
+            }
+        } catch (Exception $e) {
+            return ApiResponseHelper::error('Failed to get district data', $e->getMessage());
+        }
     }
 
-    public function show(District $district)
+    public function store(DistrictStoreRequest $request)
     {
-        //
+        try {
+            $district = $this->districtService->create($request->validated());
+            return ApiResponseHelper::success('District data has been added successfully', $district);
+        } catch (Exception $e) {
+            return ApiResponseHelper::error('Error when saving district data', $e->getMessage(), 500);
+        }
     }
 
-    public function update(Request $request, District $district)
+    public function show($district)
     {
-        //
+        $district = District::find($district);
+        if (!$district) {
+            return ApiResponseHelper::error('District not found', [], 404);
+        }
+        return ApiResponseHelper::success('District data', $district);
     }
 
-    public function destroy(District $district)
+    public function update(Request $request,  $district)
     {
-        //
+        try {
+            $district = District::find($district);
+            if (!$district) {
+                throw new Exception('District not found');
+            }
+            $this->districtService->update($district, $request->validated());
+            return ApiResponseHelper::success('District data has been updated successfully');
+        } catch (Exception $e) {
+            return ApiResponseHelper::error('Error when updating district data', $e->getMessage(), 500);
+        }
+    }
+
+    public function destroy($district)
+    {
+        try {
+            $district = District::find($district);
+            if (!$district) {
+                throw new Exception('District not found', 404);
+            }
+
+            $delete = $district->delete();
+            if (!$delete) {
+                throw new Exception('Failed to delete district data', 404);
+            }
+            return ApiResponseHelper::success('District data has been deleted successfully');
+        } catch (Exception $e) {
+            return ApiResponseHelper::error('District data failed to delete', $e->getMessage(), $e->getCode());
+        }
     }
 }
--- app/Http/Controllers/KelurahanController.php
+++ app/Http/Controllers/KelurahanController.php
@@ -2,48 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ApiResponseHelper;
+use App\Http\Requests\VillageIndexRequest;
+use App\Http\Requests\VillageStoreRequest;
+use App\Http\Requests\VillageUpdateRequest;
 use App\Models\Village;
-use Illuminate\Http\Request;
+use App\Services\VillageService;
+use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class KelurahanController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected $villageService;
+
+    public function __construct(VillageService $villageService)
     {
-        //
+        $this->villageService = $villageService;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function index(VillageIndexRequest $request)
     {
-        //
+        try {
+            $validated          = $request->validated();
+            $villageQuery       = Village::query()->filter($validated);
+            $village            = isset($validated['paginate']) && $validated['paginate'] ? $villageQuery->paginate($validated['perPage'] ?? 10) : $villageQuery->get();
+            $itemsToTransform   = $village instanceof LengthAwarePaginator ? $village->getCollection() : $village;
+            $transformedVillage = $itemsToTransform->map(function ($item) {
+                return [
+                    'id'            => $item->id,
+                    'name'          => $item->name,
+                ];
+            });
+            if ($village instanceof LengthAwarePaginator) {
+                return ApiResponseHelper::success('Village list', $village->setCollection($transformedVillage));
+            } else {
+                return ApiResponseHelper::success('Village list', $transformedVillage);
+            }
+        } catch (Exception $e) {
+            return ApiResponseHelper::error('Failed to get village data', $e->getMessage());
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Village $kelurahan)
+    public function store(VillageStoreRequest $request)
     {
-        //
+        try {
+            $village = $this->villageService->create($request->validated());
+            return ApiResponseHelper::success('Village data has been added successfully', $village);
+        } catch (Exception $e) {
+            return ApiResponseHelper::error('Error when saving village data', $e->getMessage(), 500);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Village $kelurahan)
+    public function show($village)
     {
-        //
+        $village = Village::find($village);
+        if (!$village) {
+            return ApiResponseHelper::error('Village not found', [], 404);
+        }
+        return ApiResponseHelper::success('Village data', $village);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Village $kelurahan)
+    public function update(VillageUpdateRequest $request, $village)
     {
-        //
+        try {
+            $village = Village::find($village);
+            if (!$village) {
+                throw new Exception('Village not found');
+            }
+            $this->villageService->update($village, $request->validated());
+            return ApiResponseHelper::success('Village data has been updated successfully');
+        } catch (Exception $e) {
+            return ApiResponseHelper::error('Error when updating village data', $e->getMessage(), 500);
+        }
+    }
+
+    public function destroy($village)
+    {
+        try {
+            $village = Village::find($village);
+            if (!$village) {
+                throw new Exception('Village not found', 404);
+            }
+
+            $delete = $village->delete();
+            if (!$delete) {
+                throw new Exception('Failed to delete village data', 404);
+            }
+            return ApiResponseHelper::success('Village data has been deleted successfully');
+        } catch (Exception $e) {
+            return ApiResponseHelper::error('Village data failed to delete', $e->getMessage(), $e->getCode());
+        }
     }
 }
