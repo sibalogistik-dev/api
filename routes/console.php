626bae60572fcaead920182a2791f517f80a1f44
neveleneve <pandupandu813@gmail.com>
2025-12-29 17:23:39 +0700

    Minis

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('attendance:auto-absent')
-    ->dailyAt('17:20')
+    ->dailyAt('17:25')
     ->withoutOverlapping()
     ->runInBackground();
