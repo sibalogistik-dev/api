28464dec27eb91ba84d2235ae1429b57725ac9de
neveleneve <pandupandu813@gmail.com>
2025-12-29 17:30:59 +0700

    Miniso

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('attendance:auto-absent')
-    ->dailyAt('17:25')
+    ->dailyAt('10:32')
     ->withoutOverlapping()
     ->runInBackground();
