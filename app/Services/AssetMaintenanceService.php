bbecea296f70b272c744b93f084007ff01fe4409
neveleneve <pandupandu813@gmail.com>
2025-12-30 11:54:41 +0700

    Remove commented-out transaction code from create and update methods in AssetMaintenanceService

--- app/Services/AssetMaintenanceService.php
+++ app/Services/AssetMaintenanceService.php
@@ -15,9 +15,6 @@ class AssetMaintenanceService
 {
     public function create(array $data)
     {
-        // return DB::transaction(function () use ($data) {
-        //     return AssetMaintenance::create($data);
-        // });
         $filePaths = [];
         DB::beginTransaction();
         try {
@@ -40,10 +37,6 @@ class AssetMaintenanceService
 
     public function update(AssetMaintenance $assetMaintenance, array $data)
     {
-        // return DB::transaction(function () use ($assetMaintenance, $data) {
-        //     $assetMaintenance->update($data);
-        //     return $assetMaintenance;
-        // });
         $filePaths = [];
         DB::beginTransaction();
         try {
