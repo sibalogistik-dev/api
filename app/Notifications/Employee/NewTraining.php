524bcd2f18b910bd16537020e2a8fc035a78d53f
neveleneve <pandupandu813@gmail.com>
2026-01-21 16:17:09 +0700

    fix: Update module name in NewTraining notification from 'hrd' to 'karyawan'

--- app/Notifications/Employee/NewTraining.php
+++ app/Notifications/Employee/NewTraining.php
@@ -15,7 +15,7 @@ class NewTraining extends BaseNotification
 
     protected function getModuleName(): string
     {
-        return 'hrd';
+        return 'karyawan';
     }
 
     protected function getTitle(): string
